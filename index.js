a9a6993bc42589d966b599325b4da8038902689e
BruceHill <brucehillwalley@gmail.com>
2024-03-29 17:19:38 +0300

    up

---
+++ index.js
@@ -0,0 +1,74 @@
+"use strict"
+/* -------------------------------------------------------
+    NODEJS EXPRESS | MIDNIGHT CODERS HOTEL API
+------------------------------------------------------- */
+/*
+    $ cp .env-sample .env
+    $ npm init -y
+    $ npm i express dotenv mongoose express-async-errors
+    $ npm i morgan swagger-autogen swagger-ui-express redoc-express
+    $ mkdir logs
+    $ nodemon
+*/
+const express = require('express')
+const app = express()
+
+/* ------------------------------------------------------- */
+// Required Modules:
+
+// envVariables to process.env:
+require('dotenv').config()
+const PORT = process.env?.PORT || 8000
+const HOST = process.env?.HOST || '127.0.0.1'
+
+// asyncErrors to errorHandler:
+require('express-async-errors')
+
+/* ------------------------------------------------------- */
+// Configrations:
+
+// Connect to DB:
+const { dbConnection } = require('./src/configs/dbConnection')
+dbConnection()
+
+/* ------------------------------------------------------- */
+// Middlewares:
+
+// Accept JSON:
+app.use(express.json())
+
+// Logger:
+
+
+// Auhentication:
+
+
+// findSearchSortPage / res.getModelList:
+
+
+/* ------------------------------------------------------- */
+// Routes:
+
+// routes/index.js:
+app.use('/', require('./src/routes/'))
+
+// HomePath:
+app.all('/', (req, res) => {
+    res.send({
+        error: false,
+        message: 'Welcome to HOTEL API',
+       
+    })
+})
+
+/* ------------------------------------------------------- */
+
+// errorHandler:
+app.use(require('./src/middlewares/errorHandler'))
+
+// RUN SERVER:
+app.listen(PORT, () => console.log('http://127.0.0.1:' + PORT))
+
+/* ------------------------------------------------------- */
+// Syncronization (must be in commentLine):
+// require('./src/helpers/sync')() // !!! It clear database.
